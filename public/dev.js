5fe2bb5e8cac95483b174e7fc66aa5a204d7c397
merill <merill@users.noreply.github.com>
2026-04-05 09:57:29 +1000

    Added Edge icon

--- public/dev.js
+++ public/dev.js
@@ -1,4 +1,5 @@
-chrome.devtools.panels.create("Graph X-Ray", null, "/devtools.html", function (panel) {
+// Use a higher-resolution source icon so the DevTools tab icon stays crisp when scaled.
+chrome.devtools.panels.create("Graph X-Ray", "img/icon-48.png", "/devtools.html", function (panel) {
     console.log("Graph X-Ray: ", panel);
   }
 );
